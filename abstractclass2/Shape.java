82f2908c61e7c2aaf657b12cc773d845ccfb69db
DINESH SK <dineshsrinivasan2001@gmail.com>
2022-03-27 21:43:33 +0530

    Delete abstractclass2 directory

--- abstractclass2/Shape.java
+++
@@ -1,8 +0,0 @@
-package com.assignment.corejava.abstractclass2;
-
-public abstract class Shape {
-  abstract double ReactangleArea(double length,double breath);
-  abstract double SquareArea(double side);
-  abstract double CircleArea(double radius);
-  
-}
