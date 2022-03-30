c7c02a202f6cea67609444fbaf866ee954755d42
DINESH SK <dineshsrinivasan2001@gmail.com>
2022-03-30 09:07:08 +0530

    Add files via upload

---
+++ Assignment/set/linkedhashset/SortById.java
@@ -0,0 +1,12 @@
+package com.assignment.set.linkedhashset;
+
+import java.util.Comparator;
+
+public class SortById implements Comparator<LinkedHashSetCompartor>{
+
+	@Override
+	public int compare(LinkedHashSetCompartor o1, LinkedHashSetCompartor o2) {
+		return  o1.roll_no - o2.roll_no;
+	}
+
+}
---
+++ Assignment/set/linkedhashset/SortByName.java
@@ -0,0 +1,12 @@
+package com.assignment.set.linkedhashset;
+
+import java.util.Comparator;
+
+public class SortByName implements Comparator<LinkedHashSetCompartor> {
+
+	@Override
+	public int compare(LinkedHashSetCompartor o1, LinkedHashSetCompartor o2) {
+		return o1.name.compareTo(o2.name);
+	}
+
+}
