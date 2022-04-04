04739565157e360194a0399d2a2ddffb0050d3b7
DINESH SK <dineshsrinivasan2001@gmail.com>
2022-04-04 18:52:53 +0530

    Add files via upload

---
+++ finalAssessment/GetStudent.java
@@ -0,0 +1,26 @@
+package finalAssessment;
+
+import java.util.Scanner;
+import java.util.Set;
+
+public class GetStudent {
+	public void getStudent(Set<Student> set, Scanner scanner) {
+		boolean status = true;
+		System.out.println("====================");
+		System.out.print("Enter Student Id for Details :");
+		int checkId = scanner.nextInt();
+		for (Student student : set) {
+			if (student.id == checkId) {
+				status = false;
+				System.out.println(student);
+				break;
+			}
+		}
+		if (status) {
+			System.out.println("Id Doesn't Exit");
+		} else {
+			System.out.println("!!--Successfully Getting Student Details--!!");
+		}
+		System.out.println("====================");
+	}
+}
---
+++ finalAssessment/Student.java
@@ -0,0 +1,50 @@
+package finalAssessment;
+
+public class Student {
+	int id;
+	String name;
+	int marks;
+	String course;
+
+	public Student(int id, String name, int marks, String course) {
+		this.id = id;
+		this.name = name;
+		this.marks = marks;
+		this.course = course;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", marks=" + marks + ", course=" + course + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return this.id;
+	}
+
+//	Object object = new Student();
+//	Up casting internally.
+	@Override
+	public boolean equals(Object obj) {
+		Student std = (Student) obj; // DownCasting
+		return this.id == std.id;
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public int getMarks() {
+		return this.marks;
+	}
+
+	public String getCourse() {
+		return this.course;
+	}
+
+}
